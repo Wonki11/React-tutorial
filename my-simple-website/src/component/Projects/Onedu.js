aed09d9a771ae60d6bfad447344dd7a9d0a20d06
Wonki11 <wongi5011@daum.net>
2024-07-22 09:25:44 +0900

    yarn

---
+++ my-simple-website/src/component/Projects/Onedu.js
@@ -0,0 +1,14 @@
+const  Onedu = () => {
+    <ul>
+        <li><a href="">원두쓰리포 소개</a></li>
+        <li><a href="">피그마</a></li>
+        <li><a href="">노션</a></li>
+        <li><a href="">ErdCloud</a></li>
+        <li><a href="">회의록</a></li>
+        <li><a href="">문서</a></li>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+        
+    </ul>
+}
+export default Onedu;
